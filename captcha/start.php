6e419d3feeba04c067a040af05d65cd3069bc622
Daniil <daniil.rakhmatulin@gmail.com>
2015-02-11 17:53:29 +0600

    adds autoloader

--- captcha/start.php
+++ captcha/start.php
@@ -1,5 +1,12 @@
 <?php
 
+if (!File::exists(path('base').'vendor/autoload.php'))
+{
+	throw new Exception('You need to run composer update to complete installation of this project.');
+}
+
+require path('base').'vendor/autoload.php';
+
 Autoloader::namespaces(array(
 	'Captcha' => Bundle::path('captcha')
 ));
